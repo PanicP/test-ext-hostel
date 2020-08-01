22d9c42b43de218f09ede9d814c09641349ec5c2
PanicP <thunthian.pornmuneewong@kingpower.com>
2020-08-01 15:07:33 +0700

    WIP: Add simple route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import './App.css'
 import { HomePage } from './pages'
+import { Router, Switch, Route } from 'react-router-dom'
+import { history } from './history'
 
 const App = () => {
-    return <HomePage />
+    return (
+        <Router history={history}>
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/test/" component={ () => <div></div>}/>
+            </Switch>
+        </Router>
+    )
 }
 
 export default App
