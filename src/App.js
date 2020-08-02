4a96b4438290807175a4445094bfaaf05674814d
PanicP <thunthian.pornmuneewong@kingpower.com>
2020-08-02 22:37:13 +0700

    WIP: Start fix some css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from 'react'
 import './App.css'
-import {
-    HomePage,
-    LoginPage,
-    RegisterPage,
-    HostelDetailPage,
-    HostelBookingListPage,
-} from './pages'
+import { HomePage, LoginPage, RegisterPage, HostelDetailPage, HostelBookingListPage } from './pages'
 import { Router, Switch, Route } from 'react-router-dom'
 import { history } from './history'
 import { PrivateRoute } from './components/auth'
@@ -19,11 +13,7 @@ const App = () => {
                 <Route exact path="/login" component={LoginPage} />
                 <Route exact path="/register" component={RegisterPage} />
                 <PrivateRoute exact path="/hostel/:id" component={HostelDetailPage} />
-                <PrivateRoute
-                    exact
-                    path="/hostel-booking-list"
-                    component={HostelBookingListPage}
-                />
+                <PrivateRoute exact path="/hostel-booking-list" component={HostelBookingListPage} />
             </Switch>
         </Router>
     )
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react'
 import { ClassicWithSidebarLayout } from '../components/layout'
 import { HostelList, HostelCarousel } from '../components/hostel'
+import { Space } from 'antd'
 
 export const HomePage = () => {
-
     const [isAuthed, setIsAuthed] = useState(localStorage.getItem('authToken'))
 
     return (
         <ClassicWithSidebarLayout>
-            <HostelCarousel/>
-            { isAuthed ? <HostelList></HostelList> : <div></div>}
-            <h3>Welcome to the hostel picker!</h3>
+            <HostelCarousel />
+            <Space>
+                <h3 className="homepage-welcome-text">Welcome to the hostel picker!</h3>
+            </Space>
+            {isAuthed ? <HostelList></HostelList> : <div></div>}
         </ClassicWithSidebarLayout>
     )
 }
